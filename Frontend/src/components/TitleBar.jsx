87175b91fe3f068dde7292f54d688f206ab28532
Rup Kumar Biswas <rb636739@gmail.com>
2025-07-30 22:11:23 +0530

    Delete Frontend directory

--- Frontend/src/components/TitleBar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TitleBar = () => {
-  return null;
-};
-
-export default TitleBar;
--- Frontend/start-dev.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "E:\python projects\StockPlay\Frontend"
-node node_modules\vite\bin\vite.js
-pause
